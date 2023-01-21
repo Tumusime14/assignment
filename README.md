5df1ba3fbb57a5e0e5fdab4ad2a292d739b4bce3
TUMUSIME DAVID <tumusimedavid14@gmail.com>
2023-01-20 17:26:06 -0800

    DAVID UPLOAD

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tumusime david submnission 
---
+++ information.php
@@ -0,0 +1,6 @@
+<?php
+ $Fullname="TUMUSIME David";
+ $Regno="21RP12437";
+ echo "Names: " .$Fullname."<br>";
+ echo "Registration number: ". $Regno;
+?>
